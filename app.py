b8b5ef92f8f7fa18e226fa2b14f720fca1c8344d
Khan Abid <111801271+Khan-Abid786@users.noreply.github.com>
2025-02-15 14:26:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ def load_model(model_path):
 
 # Load the trained model
 current_directory = os.path.dirname(os.path.abspath(__file__))
-model_path = os.path.join(current_directory, 'brain_tumor_modelv2.h5')
+model_path = os.path.join(current_directory, 'brain_tumor_modelv1.h5')
 st.write(f"Current directory: {current_directory}")  # Print the current directory
 model = load_model(model_path)
 
