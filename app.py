21f408c03a2ee456e3df829f33c02052cd60133f
Khan Abid <111801271+Khan-Abid786@users.noreply.github.com>
2025-02-16 15:03:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -42,10 +42,11 @@ if model is not None:
         # Predict the class of the image
         prediction = model.predict(img_array)
         confidence = prediction[0][0]
-        if confidence > 0.5:
-            return f"Yes, Brain Tumor detected with {confidence * 100:.2f}% confidence"
-        else:
-            return f"No, Brain Tumor not detected with {(1 - confidence) * 100:.2f}% confidence"
+        return confidence
+        #if confidence > 0.5:
+         #   return f"Yes, Brain Tumor detected with {confidence * 100:.2f}% confidence"
+        #else:
+       #     return f"No, Brain Tumor not detected with {(1 - confidence) * 100:.2f}% confidence"
 
     # Streamlit app
     #st.title("Welcome")
