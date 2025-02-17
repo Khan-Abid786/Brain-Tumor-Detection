a82af3848c5c4d0ea9ffa944daa8a66cf1db3361
Khan Abid <111801271+Khan-Abid786@users.noreply.github.com>
2025-02-17 15:48:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def load_model(model_path):
 
 # Load the trained model
 current_directory = os.path.dirname(os.path.abspath(__file__))
-model_path = os.path.join(current_directory, 'brain_tumor_modelv4.h5')
+model_path = os.path.join(current_directory, 'brain_tumor_modelv5.h5')
 model = load_model(model_path)
 
 if model is not None:
